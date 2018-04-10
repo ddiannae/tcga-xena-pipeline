65bd17b6dcaa10c8eb703dcee7335ff8a96b4152
dianagco <diana.gco@gmail.com>
2018-04-10 18:52:06 -0500

    Remove .short for manifests files

--- start.sh
+++ start.sh
@@ -13,8 +13,8 @@ LOGSDIR="data/logs"
 mkdir -p $LOGSDIR
 
 if [[ $DOWNLOAD == "true" ]]; then
-  gdc-client download -d $CANCERDIR -m manifests/${PRIMARY_SITE}.cancer.short.txt --log-file ${LOGSDIR}/${PRIMARY_SITE}.cancer.log --retry-amount 3
-  gdc-client download -d $HEALTHYDIR -m manifests/${PRIMARY_SITE}.healthy.short.txt --log-file ${LOGSDIR}/${PRIMARY_SITE}.healthy.log --retry-amount 3
+  gdc-client download -d $CANCERDIR -m manifests/${PRIMARY_SITE}.cancer.txt --log-file ${LOGSDIR}/${PRIMARY_SITE}.cancer.log --retry-amount 3
+  gdc-client download -d $HEALTHYDIR -m manifests/${PRIMARY_SITE}.healthy.txt --log-file ${LOGSDIR}/${PRIMARY_SITE}.healthy.log --retry-amount 3
 
   find $CANCERDIR -name '*.gz' -exec mv '{}' $CANCERDIR \;
   find $HEALTHYDIR -name '*.gz' -exec mv '{}' $HEALTHYDIR \;
