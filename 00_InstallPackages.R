345d69936c0e8b8e92185f2ceceb91a6b2cf1c0d
Diana Garcia <diana.gco@gmail.com>
2018-07-12 10:36:26 -0500

    Added EDASeq bioconductor package to 00_InstallPackages.R

--- 00_InstallPackages.R
+++ 00_InstallPackages.R
@@ -1,5 +1,5 @@
 cran <- c('ggplot2', 'reshape2')
-bioconductor <- c('NOISeq', 'DESeq2', 'BiocParallel', 'cqn')
+bioconductor <- c('NOISeq', 'EDASeq', 'DESeq2', 'BiocParallel', 'cqn')
 chooseCRANmirror(graphics=FALSE, ind=1)
 source('http://bioconductor.org/biocLite.R')
 BiocInstaller::biocLite(bioconductor, ask = FALSE, suppressUpdates = TRUE, suppressAutoUpdate = TRUE)
