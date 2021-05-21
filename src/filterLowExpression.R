76b3e7875f4b2d0a14df47cc70bd608db6cff97c
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2021-05-21 02:10:24 -0500

    Removing bug. Correcting low filter

--- src/filterLowExpression.R
+++ src/filterLowExpression.R
@@ -28,11 +28,23 @@ load(file=paste(RDATA, "raw_full.RData", sep="/"))
   exp_genes <- apply(full$M, 1, function(x) mean(x)>10)
   egtable <- table(exp_genes)
   cat("There are", egtable[[1]], "genes with mean expression count < 10", egtable[[2]], "with mean count > 10 \n")
-  
+  exp_genes <- names(exp_genes[exp_genes == TRUE])
+
   ##Filtering low expression genes
-  mean10 <- list(M = full$M[exp_genes, rownames(full$targets)], annot=full$annot[exp_genes, ], 
-                 targets=full$targets)
-  rownames(mean10$annot) <- rownames(mean10$M)
+  mean10 <- list(M = full$M[exp_genes, full$targets$id],
+                 annot = full$annot %>% filter(gene_id %in% exp_genes),
+                 targets = full$targets)
+                 
+  mean10$annot <- mean10$annot %>% arrange(gene_id)
+  mean10$M <- mean10$M[order(row.names(mean10$M)), mean10$targets$id]
+  
+  stopifnot(all(mean10$annot$gene_id == rownames(mean10$M)))
+  cat(paste0("Genes in matrix and annotation match positions \n"))
+  
+  stopifnot(all(mean10$targets$id == colnames(mean10$M)))
+  cat(paste0("Samples in matrix and annotation match positions \n"))
+  
+  row.names(mean10$annot) <- mean10$annot$gene_id
   
   cat("Saving mean10_ProteinCoding.RData \n") 
   save(mean10, file=paste(RDATA, "mean10_proteinCoding.RData", sep="/"), compress="xz")
