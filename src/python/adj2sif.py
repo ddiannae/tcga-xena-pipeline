4870aaf7b26b4f349efc7e399a3b9906387d9417
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2021-07-07 14:50:04 -0500

    Removing bug to get all interactions

--- src/python/adj2sif.py
+++ src/python/adj2sif.py
@@ -1,15 +1,26 @@
-
-# Convierte una matriz de adyacencia de ARACNe en un archivo 
-# de interacciones SIF con la matriz triangular 
 import sys
+import pandas as pd
 
+# Takes ARACNE adj and builds an undirected sif
 adjmat = sys.argv[1]
+out = sys.argv[2]
 lines = [line.rstrip('\n') for line in open(adjmat)]
+sources = []
+targets = []
+mis = []
 
-c = 0
 for l in lines:
   vals = l.split()
   name = vals[0]
-  for i in range(2*c + 1, len(vals), 2):
-    print(name + "\t" + vals[i] + "\t" + vals[i+1])
-  c = c + 1 #Elemento en la triangular
+  for i in range(1, len(vals), 2):
+    st = [name, vals[i]]
+    st.sort()
+    sources.append(st[0])
+    targets.append(st[1])
+    mis.append(vals[i+1])
+
+dict = {'source': sources, 'mi':mis, 'target' : targets}
+df = pd.DataFrame(dict)
+df.sort_values(by = ["mi"], ascending = False, inplace = True)
+df.drop_duplicates(inplace = True)
+df.to_csv(out, sep="\t", index=False)
