2c800195d464190dfe672292202ec2e9857e3773
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2021-09-25 10:55:44 -0500

    Adding arsyn param to output dir

--- workflow/scripts/aracne_matrix.py
+++ workflow/scripts/aracne_matrix.py
@@ -2,13 +2,14 @@ from MultiAracne import Aracne
 import sys
 import pathlib
 
-sys.stderr = open(snakemake.log[0], "w")
+sys.stdout = open(snakemake.log[0], "w")
 
 exp_matrix = snakemake.input[0]
 outmatrix = snakemake.output[0]
-outdir = snakemake.params[0] + "/correlation/output"
-procs = int(snakemake.threads[0])
+outdir = snakemake.params[0] + "/correlation/output_" + snakemake.params[1] + "_" + snakemake.params[2]
+procs = int(snakemake.threads)
 
+print(f"Saving files in {outdir}")
 pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
     
 ma = Aracne(exp_matrix)
